c31b989be477299a59757e85cf7a9dd9751cca9d
Swagafox <igory.udenko@gmail.com>
2022-07-03 23:47:20 +0300

    added build, shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
+  $('.pagination__item').on('click', function(){
+    $('.pagination__item').removeClass('pagination__item--active');
+    $(this).addClass('pagination__item--active');
+  });
+
+  $('.select-style').styler();
+  
+  $('.login__checkbox').styler();
+
+  $('.rating').rateYo({
+    starWidth: "11px",
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true
+  })
+
   $('.slider').slick({
     arrows: false,
     dots: true
@@ -16,4 +32,20 @@ $(function() {
       enable: false
     }
   });
+
 });
+
+$(function(){
+  $('.filters__price-value').ionRangeSlider({
+    type: 'double',
+    min: 100,
+    max: 500,
+    from: 200,
+    to: 350,
+    prefix: "$",
+    onChange: function(data){
+      $('.filters__price-from').text(data.from);
+      $('.filters__price-to').text(data.to);
+    }
+  });
+})
