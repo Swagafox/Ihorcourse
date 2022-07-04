e2cb1d154cbfe492538c6c173f3559d10f16c40d
Swagafox <igory.udenko@gmail.com>
2022-07-05 02:21:21 +0300

    first part of details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
+  $('.product-slide__thumbs').slick({
+    asNavFor: '.product-slide__main',
+    focusOnSelect: true,
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  
+  $('.product-slide__main').slick({
+    asNavFor: '.product-slide__thumbs',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $('.pagination__item').on('click', function(){
     $('.pagination__item').removeClass('pagination__item--active');
     $(this).addClass('pagination__item--active');
@@ -9,8 +26,16 @@ $(function() {
   
   $('.login__checkbox').styler();
 
+
   $('.rating').rateYo({
     starWidth: "11px",
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true
+  })
+  
+  $('.product-item__rating').rateYo({
+    starWidth: "16px",
     ratedFill: "#ffcc00",
     normalFill: "#d6d6d6",
     readOnly: true
